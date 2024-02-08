8df444c940a5498976abc668cd3d9329f117d5fe
Albert Esteve <aesteve@redhat.com>
2024-02-08 10:26:35 +0100

    hw/display: add vhost-user-media
    Add boiler plate code for vhost-user-media-pci.
    
    Example
    -device vhost-user-media-pci,chardev=media,id=media
    -chardev socket,path=media.sock,id=media
    
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

---
+++ hw/virtio/vhost-user-media.h
@@ -0,0 +1,48 @@
+/*
+ * vhost-user-media virtio device
+ *
+ * Copyright Red Hat, Inc. 2024
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef _VHOST_USER_MEDIA_H_
+#define _VHOST_USER_MEDIA_H_
+
+#include "standard-headers/linux/virtio_ids.h"
+//#include "standard-headers/linux/virtio_media.h"
+#include "hw/virtio/virtio.h"
+#include "hw/virtio/vhost.h"
+#include "hw/virtio/vhost-user.h"
+#include "chardev/char-fe.h"
+
+#define TYPE_VHOST_USER_MEDIA "vhost-user-media-device"
+#define VHOST_USER_MEDIA(obj) \
+        OBJECT_CHECK(VHostUserMEDIA, (obj), TYPE_VHOST_USER_MEDIA)
+
+struct virtio_media_config {
+    __le32 device_caps;
+    __le32 device_type;
+    __u8 card[32];
+};
+
+typedef struct {
+    CharBackend chardev;
+    struct virtio_media_config config;
+} VHostUserMEDIAConf;
+
+typedef struct {
+    /*< private >*/
+    VirtIODevice parent;
+    VHostUserMEDIAConf conf;
+    struct vhost_virtqueue *vhost_vq;
+    struct vhost_dev vhost_dev;
+    VhostUserState vhost_user;
+    VirtQueue *command_vq;
+    VirtQueue *event_vq;
+    bool connected;
+    /*< public >*/
+} VHostUserMEDIA;
+
+
+#endif /* _VHOST_USER_MEDIA_H_ */
